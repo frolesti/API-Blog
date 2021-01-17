1602112011c938429ecd93cbed2176600d53b166
veronicavc <veronicavelazquezcalleja@gmail.com>
2021-01-17 13:33:12 +0100

    logic of pagination

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import { postListComponent } from './components/postList.js'
 
 (async () => {
+    let page = 0
     const $container = $('.main-wrapper')
-    await postListComponent.render($container)
+    $('.pagination a li').on('click', function(e) { 
+        e.preventDefault()
+        page = ($(e.target).text()) -1
+        $('.pagination a li').removeClass('is-active')
+        $(this).addClass('is-active')
+        $('.wrapper-list').remove()
+        postListComponent.render($container, page)
+    })
+    
+    await postListComponent.render($container, page)
 })()
--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL = 'https://jsonplaceholder.typicode.com'
-export const LIST_LIMIT = 10
+export const LIST_LIMIT = 20
