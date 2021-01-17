64e832b4a196df1529308e20638293221202bb96
veronicavc <veronicavelazquezcalleja@gmail.com>
2021-01-17 12:15:04 +0100

    pagination

--- src/components/modal.js
+++ src/components/modal.js
@@ -7,16 +7,18 @@ export const postModalComponent = {
         `<div>
             <div id="modal" class="modal-item-post">
                 <img src="assets/media/x-mark.svg" alt="close button" id="close-modal">
+                <div class="modal--post-header">
                 <h3 class="modal--post-title">${post.title}</h3>
+                <div class="modal--post-buttons">
+                    <img class="edit" src="assets/media/editar.svg" alt="edit button">
+                    <img class="delete" src="assets/media/delete.svg" alt="delete button">
+                </div>
+                </div>
                 <p class="modal--post-user" >${user.name} - <span>${user.email}</span></p>
                 <p class="modal--post-body">${post.body}</p>
                 <div class="modal--post-comments">
                     <h4>Comments</h4>
                     <button id="load-comments">Load Comments</button>
-                </div>
-                <div class="modal--post-buttons">
-                    <img class="edit" src="assets/media/editar.svg" alt="edit button">
-                    <img class="delete" src="assets/media/delete.svg" alt="delete button">
                 </div>
             </div>
             <div class="modal-item-background">
@@ -52,9 +54,4 @@ export const postModalComponent = {
 
         $container.append($modal)
     },
-}
-
-function renderModal($thiscontainer, template, post) {
-    const html = template(post)
-    $thiscontainer.append(html)
 }
